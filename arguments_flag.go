6253ec0ceff29434c5791b07e7862e5d80106e49
taufiqkba <taufiqkurniawan9991@gmail.com>
2024-06-18 23:57:41 +0700

    learn: complete arguments & flag

--- arguments_flag.go
+++ arguments_flag.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -14,4 +15,24 @@ func main() {
 
 	args := argsRaw[1:]
 	fmt.Printf("-> %#v\n", args)
+
+	// Using flag
+	name := flag.String("name", "anonymous", "type your name")
+	age := flag.Int64("age", 25, "type you age")
+
+	flag.Parse()
+	fmt.Printf("name\t: %s\n", *name)
+	fmt.Printf("age\t: %d\n", *age)
+
+	dataName := flag.String("name", "anonymous", "type your name")
+	fmt.Println(*dataName)
+
+	// flag declare with pass reference using data store
+
+	data1 := flag.String("name", "anonymous", "type your name")
+	fmt.Println(*data1)
+
+	data2 := flag.String("gender", "male", "type your gender")
+	fmt.Println(*data2)
+
 }
