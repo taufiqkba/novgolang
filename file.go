3baad891485d384e342b148763dd47b4e5523b3c
taufiqkba <taufiqkurniawan9991@gmail.com>
2024-06-26 23:58:14 +0700

    learn: file-edit file

--- file.go
+++ file.go
@@ -31,6 +31,34 @@ func createFile() {
 	fmt.Println("==> file berhasil dibuat", path)
 }
 
+func writeFile() {
+	// buka file dengan level akses READ & WRITE
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	// tulis data ke file
+	_, err = file.WriteString("halo\n")
+	if isError(err) {
+		return
+	}
+	_, err = file.WriteString("mari belajar golang\n")
+	if isError(err) {
+		return
+	}
+
+	// simpan perubahan
+	err = file.Sync()
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("==> file berhasil di isi")
+}
+
 func main() {
 	createFile()
+	writeFile()
 }
