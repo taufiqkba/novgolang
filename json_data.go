6e2da73fcb105ad191c75ca15118b81c002da536
studywithtaufiqkba <92559266+studywithtaufiqkba@users.noreply.github.com>
2024-07-29 10:30:32 +0700

    learn: complete learn Decode & Encode JSON

--- json_data.go
+++ json_data.go
@@ -13,7 +13,7 @@ type UserJSONData struct {
 }
 
 func main() {
-	// JSON Data
+	// Decode JSON to Struct Object Variable
 	var jsonString = `{"Name": "John Wick", "Age":27}`
 	jsonData := []byte(jsonString)
 
@@ -24,4 +24,51 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
+
+	fmt.Println("user : ", data.FullName)
+	fmt.Println("age :", data.Age)
+
+	// Decode JSON to map[string]interface{} & interface{}
+	var data1 map[string]interface{}
+	json.Unmarshal(jsonData, &data1)
+
+	fmt.Println("user :", data1["Name"])
+	fmt.Println("age : ", data1["Age"])
+
+	var data2 interface{}
+	json.Unmarshal(jsonData, &data2)
+
+	decodeData := data2.(map[string]interface{})
+	fmt.Println("user :", decodeData["Name"])
+	fmt.Println("age :", decodeData["Age"])
+
+	// Decode JSON Array to Obeject Array
+
+	jsonString2 := `[
+		{"Name": "John Wick", "Age": 27},
+		{"Name": "Ethan Hunt", "Age": 30}
+	]`
+
+	var data3 []UserJSONData
+
+	err2 := json.Unmarshal([]byte(jsonString2), &data3)
+	if err2 != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("user 1:", data3[0].FullName)
+	fmt.Println("user 2:", data3[1].FullName)
+
+	// Encode Object to JSON String
+	object := []UserJSONData{{"john wick", 27}, {"ethan hunt", 32}}
+	jsonData2, err := json.Marshal(object)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	jsonString2 = string(jsonData2)
+	fmt.Println(jsonString2)
+
 }
