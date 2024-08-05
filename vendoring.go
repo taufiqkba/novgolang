ee5db4ee5df0fe3b8105054e2f714e63ea2d58e0
taufiqkba <taufiqkurniawan9991@gmail.com>
2024-08-05 11:43:19 +0700

    learn: vendoring for using 3rd party library on Go

---
+++ vendoring.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/novalagung/gubrak"
+)
+
+func main() {
+	fmt.Println("Vendoring on Go!")
+	fmt.Println("")
+
+	// example implement vendoring
+	fmt.Println(gubrak.RandomInt(10, 20))
+
+}
